71a6984b843069429bbe562f86694a123e2d58b8
jcast08 <jhoancamilo0812@hotmail.com>
2024-04-23 20:55:09 -0500

    23/04/2024

---
+++ assets/js/trabajo.js
@@ -0,0 +1,8 @@
+let formulario =document.querySelector("#primerPunto")
+formulario.addEventListener('submit',function(evento){
+evento.preventDefault()
+let numeroUsuario = parseInt(evento.target.inputnumero.value)
+let total 
+
+
+})
