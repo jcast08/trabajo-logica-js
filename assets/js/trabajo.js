a49648def4a010bfd7124455f7b85bbc688ef9b9
jcast08 <jhoancamilo0812@hotmail.com>
2024-04-24 21:35:19 -0500

    24/04/2024

--- assets/js/trabajo.js
+++ assets/js/trabajo.js
@@ -1,8 +1,42 @@
-let formulario =document.querySelector("#primerPunto")
-formulario.addEventListener('submit',function(evento){
-evento.preventDefault()
-let numeroUsuario = parseInt(evento.target.inputnumero.value)
-let total 
+let formulario = document.querySelector("#primerPunto")
+formulario.addEventListener('submit', function (evento) {
+    evento.preventDefault()
+    let numeroUsuario = evento.target.inputnumero.value
+    let total = 0
 
+    for (x = 1; x <= numeroUsuario; x++) {
+        total = total + x
+    }
+
+    document.querySelector("#resultado").innerHTML = total
+
+})
+
+
+let formulario2 = document.querySelector("#segundoPunto")
+formulario2.addEventListener('submit', function (evento) {
+    evento.preventDefault()
+    let numeroUsusario2 = evento.target.inputDos.value
+    if (numeroUsusario2 % 2 == 0) {
+        document.querySelector("#resultadoDos").innerHTML = "Es par"
+    }
+    else {
+        document.querySelector("#resultadoDos").innerHTML = "Es impar"
+    }
+
+})
+
+
+let formulario3 = document.querySelector("#tercerPunto")
+formulario3.addEventListener('submit', function (evento) {
+    evento.preventDefault()
+
+    let numeroUsuario3 = evento.target.inputTres.value
+    let generohm = evento.target.generos.value
+
+    if (numeroUsuario3 <= 10) { document.querySelector("#premio").innerHTML = "reclame un jugo" }
+    else if (numeroUsuario3 >= 18) { document.querySelector("#premio").innerHTML = "reclame una cerveza" }
+
+    if(generohm == "mujer"){document.querySelector("#premio_2").innerHTML = "reclame pizza hawaiana"}
 
 })
