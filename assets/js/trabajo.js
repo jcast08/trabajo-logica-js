12c9c9ab5c221805e5be1f48d6874f89f6acbb73
jcast08 <jhoancamilo0812@hotmail.com>
2024-05-02 21:47:03 -0500

    02/05/2024

--- assets/js/trabajo.js
+++ assets/js/trabajo.js
@@ -68,3 +68,85 @@ formulario4.addEventListener('submit', function (evento) {
     }
 
 })
+
+
+
+//Punto numero 5
+
+let formulario5 = document.querySelector("#QuintoPunto")
+formulario5.addEventListener('submit',function(evento){
+evento.preventDefault()
+
+let Platos = [{
+    id: 1,
+    nombre: "Hamburguesa",
+    precio: 15000,
+    cantidad: 1
+},
+{
+    id: 2,
+    nombre: "Perro Caliente",
+    precio: 13000,
+    cantidad: 1
+},
+{
+    id: 3,
+    nombre: "Empanada",
+    precio: 3000,
+    cantidad: 1
+},
+{
+    id: 4,
+    nombre: "Papa Rellena",
+    precio: 150000,
+    imagen: "https://via.placeholder.com/100x100",
+    cantidad: 1
+},
+
+{
+    id: 4,
+    nombre: "Arepa de Queso",
+    precio: 2000,
+    cantidad: 1
+}
+
+]
+
+let Bebidas = [{
+    id: 1,
+    nombre: "Colombiana",
+    precio: 3000,
+    cantidad: 1
+},
+{
+    id: 2,
+    nombre: "Uva",
+    precio: 3000,
+    cantidad: 1
+},
+{
+    id: 3,
+    nombre: "Cocacola",
+    precio: 3500,
+    cantidad: 1
+},
+{
+    id: 4,
+    nombre: "Limonada",
+    precio: 4000,
+    imagen: "https://via.placeholder.com/100x100",
+    cantidad: 1
+},
+
+{
+    id: 4,
+    nombre: "Agua",
+    precio: 2000,
+    cantidad: 1
+}
+
+]
+
+
+
+})
